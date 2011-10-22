6361ae49ccdacb766a3ab1c15cd62f0ccd31d163
Jason Evanish <evanish.j@gmail.com>
2011-10-22 15:04:17 -0400

    created a rails 3 app with users scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstRailsApp::Application.config.secret_token = 'a9a39f96e0bb6f8d6880edee9e8fa545041f586bb581e421f4ed435642666638c6d962eb1e6a05987301233c6ececc99bc951fde679309a8923e026bb269a616'
